221bd81d7a40e804001c02d63a4a794d5357a024
bhupeshp <bhupesh.punch@gmail.com>
2015-11-14 00:44:51 +0800

    Update BGPlugin.java

--- src/android/BGPlugin.java
+++ src/android/BGPlugin.java
@@ -45,6 +45,7 @@ public class BGPlugin extends CordovaPlugin {
         }
         catch(Exception err) {
             Toast.makeText(this.cordova.getActivity().getApplicationContext(), err.toString(), Toast.LENGTH_SHORT).show();
+            callbackContext.error("Invalid action");
             return false;
         }
 

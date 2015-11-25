ec211c2c6abfb51d7d63e8be73c67b95d616fdb1
bhupeshp <bhupesh.punch@gmail.com>
2015-11-26 03:10:51 +0800

    Update BGPlugin.java

--- src/android/BGPlugin.java
+++ src/android/BGPlugin.java
@@ -1,21 +1,16 @@
 package com.qt.app.common;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.util.Log;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * Created by SAMSUNG on 11/13/2015.
- */
 public class BGPlugin extends CordovaPlugin {
 
     public static final String ACTION_ADD_BGSERVICE = "addBGService";
@@ -23,41 +18,37 @@ public class BGPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
-        Log.d("QTBGPlugin", "inside execute of plugin");
+
+        Log.d("QTBGPlugin", "inside execute");
 
         try {
             if (ACTION_ADD_BGSERVICE.equals(action)) {
-                Log.d("QTBGPlugin", "inside try");
+                Log.d("QTBGPlugin", "inside execute");
                 JSONObject arg_object = args.getJSONObject(0);
-                String accountName = arg_object.getString("accountName");
-                String pwd = arg_object.getString("accountPwd");
-                String serverURL = arg_object.getString("serverURL");
-                
-                Log.d("QTBGPlugin", "inside try"+accountName+pwd+serverURL);
+                String contactId = arg_object.getString("contactid");
 
                 Context context=this.cordova.getActivity().getApplicationContext();
-                // use this to start and trigger a service
-                Intent i= new Intent(this.cordova.getActivity(), BGService.class);
-                Log.d("QTBGPlugin", "inside try created intent");
-                // potentially add data to the intent
-                i.putExtra("accountName", accountName);
-                i.putExtra("pwd",pwd);
-                i.putExtra("serverURL",serverURL);
-                Log.d("QTBGPlugin", "inside try after putExtra");
-                this.cordova.getActivity().startService(i);
-                Log.d("QTBGPlugin", "inside before return");
-                return true;
+                Cursor qCursor = context.getContentResolver().query(ContactsContract.Data.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + "='vnd.android.cursor.item/QT'", new String[]{contactId}, null);
+
+                if(qCursor!=null && qCursor.getCount()>0){
+                    qCursor.close();
+                    callbackContext.success();
+                    return true;
+                }
+                qCursor.close();
+                callbackContext.success();
+                return false;
             }
             callbackContext.error("Invalid action");
             return false;
         }
         catch(Exception err) {
             Log.d("QTBGPlugin", "inside catch");
-            Toast.makeText(this.cordova.getActivity().getApplicationContext(), err.toString(), Toast.LENGTH_SHORT).show();
-            callbackContext.error("Error in BGPlugin" + err.toString());
+            callbackContext.error("Error in BGPlugin" + err);
             return false;
         }
 
     }
+
 }

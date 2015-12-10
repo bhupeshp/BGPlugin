a94e21f20bacc2a6c45ac07473da7828a6ba6ee4
bhupeshp <bhupesh.punch@gmail.com>
2015-12-11 01:24:44 +0800

    Update BGPlugin.java

--- src/android/BGPlugin.java
+++ src/android/BGPlugin.java
@@ -1,8 +1,10 @@
 package com.qt.app.common;
 
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
 import org.apache.cordova.CallbackContext;
@@ -12,11 +14,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by SAMSUNG on 11/13/2015.
+ * Created by QTTeam on 11/13/2015.
  */
 public class BGPlugin extends CordovaPlugin {
 
     public static final String ACTION_ADD_BGSERVICE = "addBGService";
+    private Uri QUERY_URI = ContactsContract.Contacts.CONTENT_URI;
+    private String CONTACT_ID = ContactsContract.Contacts._ID;
+    private String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+    private Uri EMAIL_CONTENT_URI = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+    private String EMAIL_CONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+    private String EMAIL_DATA = ContactsContract.CommonDataKinds.Email.DATA;
+    private String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+    private String PHONE_NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+    private Uri PHONE_CONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+    private String PHONE_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+    private Uri CONTENT_DATA_URI = ContactsContract.Data.CONTENT_URI;
+    private ContentResolver contentResolver;
 
 
     @Override
@@ -26,24 +40,25 @@ public class BGPlugin extends CordovaPlugin {
 
         try {
             if (ACTION_ADD_BGSERVICE.equals(action)) {
+
+                contentResolver = this.cordova.getActivity().getApplicationContext().getContentResolver();
                 Log.d("QTBGPlugin", "inside execute");
-                JSONObject arg_object = args.getJSONObject(0);
-                String contactId = arg_object.getString("contactId");
 
-                Context context=this.cordova.getActivity().getApplicationContext();
-                Cursor qCursor = context.getContentResolver().query(ContactsContract.Data.CONTENT_URI, null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + "='vnd.android.cursor.item/QT'", new String[]{contactId}, null);
 
-                if(qCursor!=null && qCursor.getCount()>0){
-                    qCursor.close();
-                    callbackContext.success();
-                    return true;
+                JSONArray contactList = new JSONArray();
+                String[] projection = new String[]{CONTACT_ID, DISPLAY_NAME, HAS_PHONE_NUMBER};
+                Cursor cursor = contentResolver.query(QUERY_URI, projection, null, null,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
+
+                while (cursor.moveToNext()) {
+                    contactList.put(getContact(cursor));
                 }
-                qCursor.close();
-                callbackContext.error("not_found");
-                return false;
+                callbackContext.success(contactList);
+                cursor.close();
+                return true;
             }
-            callbackContext.error("Invalid_action");
+
+            callbackContext.error("not_found");
             return false;
         }
         catch(Exception err) {
@@ -53,5 +68,51 @@ public class BGPlugin extends CordovaPlugin {
         }
 
     }
+
+    private JSONObject getContact(Cursor cursor) {
+        String contactId = cursor.getString(cursor.getColumnIndex(CONTACT_ID));
+        String name = (cursor.getString(cursor.getColumnIndex(DISPLAY_NAME)));
+
+        String phones = getPhone(cursor, contactId);
+        String qtFlag = getQTFlag(cursor, contactId);
+
+        JSONObject contact = new JSONObject();
+        try {
+            contact.put("contactid", contactId);
+            contact.put("name", name);
+            contact.put("phones", phones);
+            contact.put("qtFlag", qtFlag);
+        }
+        catch(Exception err) {
+            Log.d("QTBGPlugin", "inside getContact catch" + err);
+        }
+        Log.d("QTBGPlugin", "getContact : "+contact.toString());
+        return contact;
+    }
+
+    private String getPhone(Cursor cursor, String contactId) {
+        String phoneNumbers = "";
+        int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
+        if (hasPhoneNumber > 0) {
+            Cursor phoneCursor = contentResolver.query(PHONE_CONTENT_URI, null, PHONE_CONTACT_ID + " = ?", new String[]{contactId}, null);
+            while (phoneCursor.moveToNext()) {
+                String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(PHONE_NUMBER));
+                phoneNumbers += phoneNumber + ",";
+            }
+            phoneNumbers = phoneNumbers.substring(0,phoneNumbers.length());
+            phoneCursor.close();
+        }
+        return phoneNumbers;
+    }
+
+    private String getQTFlag(Cursor cursor, String contactId) {
+        Cursor qtCursor = contentResolver.query(ContactsContract.Data.CONTENT_URI, null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + "='vnd.android.cursor.item/QT'", new String[]{contactId}, null);
+        if(qtCursor.getCount()>0)
+            return "1";
+        return "0";
+    }
+
+
 
 }
